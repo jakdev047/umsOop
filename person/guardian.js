d13c55346d0b1c6b98c19ed752c28ea55c21c99b
jakdev047 <jakdevfreelancer047@gmail.com>
2021-02-02 22:20:35 +0600

    uniPerson & guardian class add

---
+++ person/guardian.js
@@ -0,0 +1,36 @@
+const Person = require("./person");
+
+const _profession = Symbol('profession');
+const _income = Symbol('income');
+
+class Guardian extends Person {
+    constructor(id, name, profession, income) {
+        super(id, name);
+        this[_profession] = profession;
+        this[_income] = income;
+    }
+
+    get profession() {
+        return this[_profession];
+    }
+
+    set profession(value) {
+        return this[_profession] = value;
+    }
+
+    get _income() {
+        return this[__income];
+    }
+
+    set _income(value) {
+        return this[__income] = value;
+    }
+
+    toString() {
+        super.toString();
+        console.log(`Profession = ${this[_profession]}`);
+        console.log(`Income = ${this[_income]}`);
+    }
+};
+
+module.exports = Guardian;
---
+++ person/index.js
@@ -0,0 +1,9 @@
+const Person = require('./person');
+const UniPerson = require('./uniPerson');
+const Guardian = require('./guardian');
+
+module.exports = {
+    Person,
+    UniPerson,
+    Guardian
+};
